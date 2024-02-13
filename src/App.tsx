1d131462479dd94b69ee67117f102d3ca095f9f0
lucianosants <lucianosantos.developer@gmail.com>
2024-02-13 17:05:32 -0300

    feat(init): implements react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
+import { Outlet } from 'react-router-dom';
+
 function App() {
 	return (
-		<div className="bg-slate-900 text-primary-foreground min-h-screen">
-			<div className="flex justify-center pt-16">
-				<h2 className="font-black text-xl">
-					Global States with Redux vs Zustand.
-				</h2>
-			</div>
+		<div className="min-h-screen bg-slate-900 text-primary-foreground">
+			<Outlet />
 		</div>
 	);
 }
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+export function Home() {
+	return (
+		<div className="min-h-screen bg-slate-900 text-primary-foreground">
+			<div className="flex justify-center pt-16">
+				<h2 className="text-xl font-black">
+					Global States with Redux vs Zustand.
+				</h2>
+			</div>
+		</div>
+	);
+}
